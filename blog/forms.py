ff01315593dd24fdfa627083b7159fe70153364f
Francisco Guilherme <frcog.monteiro@gmail.com>
2024-12-20 10:20:55 -0300

    comit

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,46 @@
 from django import forms
-from .models import Mensagem
+from .models import Mensagem, Post
 from django.core.exceptions import ValidationError
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
 
 class MensagemForm(forms.ModelForm):
     class Meta:
         model = Mensagem
         fields = ['nome', 'email', 'telefone', 'cidade', 'mensagem']
         
-def clean_ciade(self):
-    data = self.cleaned_data["cidade"]
-    cidades_validas = ["SPP", "São Pedro", "Bom Jesus"]
-    
-    if not data in cidades_validas:
-        raise ValidationError("Não aceitamos a sua cidade.")
-    
-    return data
+    def clean_ciade(self):
+        data = self.cleaned_data["cidade"]
+        cidades_validas = ["SPP", "São Pedro", "Bom Jesus"]
+        
+        if not data in cidades_validas:
+            raise ValidationError("Não aceitamos a sua cidade.")
+        
+        return data
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('nome', css_class='col-md-6'),
+                Column('email', css_class='col-md-6'),
+                css_class='row'
+            ),
+            Row(
+                Column('telefone', css_class='col-md-6'),
+                Column('cidade', css_class='col-md-6'),
+                css_class='row'
+            ),
+            Row(
+                Column('mensagem', css_class='col-12'),
+                css_class='row'
+            ),
+            Submit('submit', 'Enviar', css_class='btn btn-primary text-uppercase')
+        )
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Blog, Mensagem
-from .forms import MensagemForm
+from .forms import MensagemForm, PostForm
 from django.forms.models import model_to_dict
 
 def index(request):
@@ -31,13 +31,13 @@ def contact(request):
     if request.method == "POST":
         form = MensagemForm(request.POST)
         if form.is_valid():
-            if form.is_valid():
-                form.save()
-                return redirect('mensagens')
-            else: context["form"] = form
+            form.save()
+            return redirect('mensagens')
+        else:
+            context["form"] = form
     else:
         context["form"] = MensagemForm()
-        
+       
     return render(request, "contact.html", context)
     
 def mensagens(request):
@@ -50,10 +50,10 @@ def editar_mensagem(request, mensagem_id):
     mensagem = get_object_or_404(Mensagem, pk=mensagem_id)
     context = {
         "blog": Blog.objects.first,
-        "form": MensagemForm(initial=model_to_dict(mensagem))
+        "form": MensagemForm(instance=mensagem)
     }
     if request.method == "POST":
-        form = MensagemForm(request.POST)
+        form = MensagemForm(request.POST, instance=mensagem)
         if form.is_valid():
             form.save()
             mensagem.save()
@@ -79,3 +79,47 @@ def register(request):
         "blog": Blog.objects.first(),
     }
     return render(request, "register.html", context)
+
+def new_post(request):
+    context = {
+        "blog": Blog.objects.first(),
+    }
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('new_post')
+        else:
+            context["form"] = form
+    else:
+        context["form"] = PostForm()
+       
+    return render(request, "new_post.html", context)
+
+def editar_post(request, post_id):
+    poste = get_object_or_404(Mensagem, pk=post_id)
+    context = {
+        "blog": Blog.objects.first,
+        "form": MensagemForm(instance=poste)
+    }
+    if request.method == "POST":
+        form = MensagemForm(request.POST, instance=poste)
+        if form.is_valid():
+            form.save()
+            poste.save()
+            return redirect('post')
+        
+    return render(request, "new_post.html", context)
+
+def deletar_mensagem(request, post_id):
+    context = {
+        "blog": Blog.objects.first,
+        "post": get_object_or_404(Mensagem, pk=post_id)
+    }
+    
+    if request.method == 'POST':
+        context["post"].delete()
+        return redirect('index')
+    else:
+        return render(request, "delete_post.html", context)
